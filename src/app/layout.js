a5e65fbb476e38133eeabd9b4fe285f9f91c3463
kumarshubhh <subhanshukumar290@gmail.com>
2025-04-15 13:08:08 +0530

    midileware changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,6 +34,7 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
-    </AppContextProvider></ClerkProvider>
+    </AppContextProvider>
+    </ClerkProvider>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
+// src/middleware.ts
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    '/((?!.+\\.[\\w]+$|_next).*)', // This includes all routes except static files and _next
-    '/api/(.*)',                  // Include all API routes
+    "/((?!.*\\..*|_next).*)",
+    "/api/(.*)",
   ],
 };
